14e8dd768c03e80c35b5c93696e6019a8797f52e
David Mejia <davidndy@gmail.com>
2018-02-04 14:18:38 -0500

    Model validation an assosiations set up

---
+++ db/migrate/20180204185818_create_expenses.rb
@@ -0,0 +1,14 @@
+class CreateExpenses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :expenses do |t|
+      t.string :type_of_trans,
+      t.string :concept,
+      t.string :category,
+      t.date :date,
+      t.number :amount,
+      t.belongs_to :user,
+
+      t.timestamps
+    end
+  end
+end
