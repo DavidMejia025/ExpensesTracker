96149d7060106b3278fead17fad7aff3df04f06e
David Mejia <davidndy@gmail.com>
2018-02-04 15:06:14 -0500

    Migration and seeds

--- db/migrate/20180204185818_create_expenses.rb
+++ db/migrate/20180204185818_create_expenses.rb
@@ -1,12 +1,12 @@
 class CreateExpenses < ActiveRecord::Migration[5.0]
   def change
     create_table :expenses do |t|
-      t.string :type_of_trans,
-      t.string :concept,
-      t.string :category,
-      t.date :date,
-      t.number :amount,
-      t.belongs_to :user,
+      t.string :type_of_trans
+      t.string :concept
+      t.string :category
+      t.date :date
+      t.integer :amount
+      t.belongs_to :user
 
       t.timestamps
     end
