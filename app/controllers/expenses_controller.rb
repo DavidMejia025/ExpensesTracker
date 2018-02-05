ea745178400cd14c2a1678092a5381579f8cb19c
David Mejia <davidndy@gmail.com>
2018-02-05 08:56:18 -0500

    List of expenses, average, count and total amount

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,5 +2,6 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
   def index
     @tab = :expenses
+    @expenses = Expense.where(user_id: current_user.id)
   end
 end
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,35 @@
 module ExpensesHelper
+
+	def month_format(date_month)
+		month = {1 => "Jan",
+				 2 => "Feb",
+				 3 => "Mar",
+				 4 => "Apr",
+				 5 => "May",
+				 6 => "jun",
+				 7 => "Jul",
+				 8 => "Aug",
+				 9 => "Sep",
+				 10 => "Oct",
+				 11 => "Nov",
+				 12 => "Dic"}
+		month[date_month].upcase
+	end
+
+	# I want to learn how to pick an specific row in rails
+	#sum = Expense.where(user_id: 2).select("amount")
+
+	def summ(expenses)
+		total=expenses.reduce(0) do |accu, expense|
+			accu+=expense.amount
+		end
+		total
+	end
+
+	def average(total,expenses)
+		divider = expenses.count
+		(total/divider)
+	end
+
+
 end
