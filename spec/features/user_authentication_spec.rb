fd788a2ec742ef14117ce09e18e93c356b83f362
DavidMejia025 <davidndy@gmail.com>
2018-04-03 16:00:35 -0500

    capybara testing

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -17,6 +17,12 @@ Capybara.current_driver = :selenium_chrome
 # end
 
 RSpec.describe "user_authentication", type: :request do
+   #User.create(email:"juan@one.com", password:"123456")
+   User.create(email:"luna@one.com", password:"test1234")
+   #Category.create(name:"food")
+   #TypeOfTran.create(name:"withdraw")
+  # Expense.create(concept:"food", date:06/06/2014, amount:120000, user_id:1)
+
   describe "authenticate user" do
     it "log in" do
       get new_user_session_path
@@ -59,18 +65,28 @@ RSpec.describe "the signin process", :type => :feature do
   # end
 
   it "Create expense" do
-
+    Category.create(name:"Toys")
+    TypeOfTran.create(name:"Purchase")
     visit expenses_path
-
     fill_in "user_email", with: "luna@one.com"
     fill_in "user_password", with: "test1234"
-    sleep 5
     find('input[type="submit"]').click
-    sleep 5
     visit expenses_path
-    sleep 5
-
-    find('button[type="button"]').click
-
+    find('button.btn.btn-success.pull-left').click
+    select "2018", :from => "expense_date_1i"
+    select "April", :from => "expense_date_2i"
+    select "2", :from => "expense_date_3i"
+    type_of_tran_id = TypeOfTran.last.id
+    within '#expense_type_of_tran_id' do
+      find("option[value='#{type_of_tran_id}']").click
+    end
+    cat_id = Category.last.id
+    within '#expense_category_id' do
+      find("option[value='#{cat_id}']").click
+    end
+    fill_in "expense_concept", with: "Fish"
+    fill_in "expense_amount", with: 25000
+    find('input[type="submit"]').click
+    sleep 10
   end
 end
