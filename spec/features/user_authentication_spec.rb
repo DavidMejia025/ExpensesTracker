747e9df078a6449fba459e3130ca998d16fdef65
DavidMejia025 <davidndy@gmail.com>
2018-04-03 12:18:49 -0500

    Rspec examples model test, and integration test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -3,16 +3,24 @@ require "rails_helper"
 
 RSpec.describe "user_authentication", type: :request do
   describe "authenticate user" do
-  it{
-    get new_user_session_path
-    expect(response).to render_template(:new)
+    it "log in" do
+      get new_user_session_path
+      expect(response).to render_template(:new)
+      post user_session_path, user:{email:"juan@one.com", password:"123456"}
+      expect(response).to redirect_to(root_path)
+      follow_redirect!
+      expect(response).to render_template(:index)
+    end
 
-    post user_session_path, user:{email:"juan@one.com", password:"123456"}
-    expect(response).to redirect_to(:expenses=> :index)
-    follow_redirect!
-
-  }
-end
-
-
+    it "test expenses path" do
+      get new_user_session_path
+      expect(response).to render_template(:new)
+      post user_session_path, user:{email:"juan@one.com", password:"123456"}
+      expect(response).to redirect_to(root_path)
+      follow_redirect!
+      expect(response).to render_template(:index)
+      get expenses_path
+      
+    end
   end
+end
